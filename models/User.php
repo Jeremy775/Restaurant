4f0753ba00abdb4e3556358a3da9bd810a3a83fe
Jeremy775 <jlaisne77@gmail.com>
2022-10-03 10:51:18 +0200

    construct, storeUser, userExists

--- models/User.php
+++ models/User.php
@@ -1,40 +1,38 @@
 <?php 
+require_once "models/Database.php";
 
 class User {
 
-    private $name;
-    private $pass;
-    private $passRepeat;
-    private $mail;
-
-    public function storeUser()
+    public function __construct()
     {
-        if (isset($_POST['name']) && isset($_POST['pass']) && isset($_POST['passrepeat']) && isset($_POST['mail'])) {
-            $this->name = strip_tags($_POST['name']);
-            $this->pass = $_POST['pass'];
-            $this->passRepeat = $_POST['passrepeat'];
-            $this->mail = $_POST['mail'];
+        $db = new Database();
+        $this->conn = $db->getPdo();
+    }
 
-            if (!$this->pass === $this->passRepeat) {
-                die("Les mots de passe de correspondent pas");
-            }
+    public function storeUser($name, $pass, $mail)
+    {
+        $query = "INSERT INTO  users(user_name, user_pass, user_mail) VALUES (:name, :pass, :mail)";
 
-            $passHash = password_hash($this->pass, PASSWORD_DEFAULT);
+        $sql = $this->con->query($query);
 
-            require_once "Database.php";
+        $sql->bindValue(":name", $name, PDO::PARAM_STR);
+        $sql->bindValue(":pass", $pass, PDO::PARAM_STR);
+        $sql->bindValue(":mail", $mail, PDO::PARAM_STR);
 
-            $sql = "INSERT INTO `users`(`user_name`, `user_pass`, `user_mail`) VALUES (:name, :pass, :mail)";
+        $sql->execute();
 
-            $query = $pdo->prepare($sql);
+    }
 
-            $query->bindValue(":name", $this->name, PDO::PARAM_STR);
-            $query->bindValue(":pass", $passHash, PDO::PARAM_STR);
-            $query->bindValue(":mail", $this->mail, PDO::PARAM_STR);
-
-            $query->execute();
-
-        } else {
-            die('Le formulaire est incomplet');
+    public function userExists($mail)
+    {
+        $checkUser = "SELECT mail from users WHERE mail='$mail' LIMIT 1";
+        $res = $this->conn->query($checkUser);
+        if ($res->num_row > 0)
+        {
+            return true;
+        }else {
+            return false;
         }
     }
+
 }

62eaef1cac5b1845d97e3d981f622ac13634f3a8
Jeremy775 <jlaisne77@gmail.com>
2022-10-03 10:50:35 +0200

    signupUser

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,17 +1,30 @@
 <?php 
-require_once "models/User.php";
+include_once ("models/User.php");
 
 class UserController extends User {
-    public function __construct($name, $pass, $passRepeat, $mail)
-    {
-        $this->name = $name;
-        $this->pass = $pass;
-        $this->passRepeat = $passRepeat;
-        $this->mail = $mail;
-    }
 
     public function signupUser()
     {
-        $this->storeUser($this->name, $this->pass, $this->mail);
+        if (isset($_POST['name']) && isset($_POST['pass']) && isset($_POST['passrepeat']) && isset($_POST['mail'])) {
+            $name = strip_tags($_POST['name']);
+            $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $passRepeat = $_POST['passrepeat'];
+            $mail = $_POST['mail'];
+
+            if ($pass !== $passRepeat) {
+                die("Les mots de passe ne correspondent pas");
+            }
+
+            if($this->userExists($mail))
+            {
+                die("Cet utilisateur existe déjà");
+            }
+
+            $this->storeUser($name, $pass, $mail);
+
+        } else {
+            die('Le formulaire est incomplet');
+        }
+        
     }
 }
